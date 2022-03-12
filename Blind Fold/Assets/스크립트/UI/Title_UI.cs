cb846358daa69ef93dc88abcdd7fbadfc54f9dc1
JiWonKimP <86395330+JiWonKimP@users.noreply.github.com>
2022-03-12 16:41:35 +0900

    22/03/12-02 UI 추가작업
    튜토리얼 씬 추가
    일시정지 기능 추가
    esc키로 일시정지 가능
    메인메뉴 돌아가기 미구현
    (경고창 유무 정하고 작업할 것)

---
+++ Blind Fold/Assets/스크립트/UI/Title_UI.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class Title_UI : MonoBehaviour
+{
+    public GameObject SettingPanel;
+    public GameObject Play_btn;
+    public GameObject Tutorial_btn;
+    public GameObject Setting_btn;
+    public GameObject Exit_btn;
+
+    public void PlayEnter()
+    {
+        //Play버튼 누를 시 PlayScene으로 이동
+        SceneManager.LoadScene("PlayScene");
+    }
+
+    public void TutorialEnter()
+    {
+        //Tutorial버튼 누를 시 TutorialScene으로 이동
+        SceneManager.LoadScene("TutorialScene");
+    }
+
+    public void SettingEnter()
+    {
+        //Setting버튼 누를 시 SettingPanel나옴
+        SettingPanel.SetActive(true);
+    }
+
+    public void ExitEnter()
+    {
+        Application.Quit();
+    }
+}
