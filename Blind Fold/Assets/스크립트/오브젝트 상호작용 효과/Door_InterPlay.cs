32a3973fccd3c4089d839806b925c0a6d45ad7d4
tjdqls1258 <42854743+tjdqls1258@users.noreply.github.com>
2022-03-22 00:11:23 +0900

    03-22 곽성빈 튜토리얼 문 + 빛 배치
    튜토리얼 여닫이 문 + 빛 배치

--- Blind Fold/Assets/스크립트/오브젝트 상호작용 효과/Door_InterPlay.cs
+++ Blind Fold/Assets/스크립트/오브젝트 상호작용 효과/Door_InterPlay.cs
@@ -5,18 +5,27 @@ using UnityEngine;
 public class Door_InterPlay : MonoBehaviour, I_Interplay_effect
 {
     Animator animator;
-    bool It_Close = true;
+    [SerializeField] bool It_Close = true;
     private void Awake()
     {
         //해당 컴포넌트를 Interplay_machice에 참조시킨다.
         animator = GetComponent<Animator>();
         GetComponent<Interplay_machice>().SetInterplay(this);
+        animator.SetBool("SwitchDoor", It_Close);
+        if (!It_Close)
+        {
+            GetComponent<Interplay_machice>().Exposition = "문 열기";
+        }
+        else
+        {
+            GetComponent<Interplay_machice>().Exposition = "문 닫기";
+        }
     }
 
     public void Effect()
     {
         animator.SetBool("SwitchDoor", It_Close);
-        if (It_Close)
+        if (!It_Close)
         {
             GetComponent<Interplay_machice>().Exposition = "문 열기";
         }
