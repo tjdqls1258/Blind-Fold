5540dfb94faba5ce2071e85a7dfb0cf23375a62e
tjdqls1258 <42854743+tjdqls1258@users.noreply.github.com>
2022-04-10 17:45:55 +0900

    04-10 곽성빈 돌 2개 추가
    돌 2개 추가

--- Assets/스크립트/Player/Fire_Bullte.cs
+++ Assets/스크립트/Player/Fire_Bullte.cs
@@ -5,12 +5,12 @@ using UnityEngine;
 public class Fire_Bullte : MonoBehaviour
 {
     [Header("Bullte Setting")]
-    [SerializeField] GameObject Bullte;
-    [SerializeField] Transform fir_Pos;
+    public GameObject Bullte;
+    [SerializeField] private Transform fir_Pos;
     [Header("InGame Shotting Setting")]
-    [SerializeField] int Count_Bullte = 5;
-    [SerializeField] float Delay = 1.0f;
-    bool Can_Fire = true;
+    [SerializeField] private int Count_Bullte = 0;
+    [SerializeField] private float Delay = 1.0f;
+    private bool Can_Fire = true;
     //총알 발사
     private void Update()
     {
@@ -31,12 +31,14 @@ public class Fire_Bullte : MonoBehaviour
         Can_Fire = true;
     }
 
-    public void add_Count_Bullte(int count)
+    public bool add_Count_Bullte()
     {
-        Count_Bullte = Count_Bullte + count;
+        if (Count_Bullte == 0)
+        {
+            Count_Bullte = 1;
+            return true;
+        }
+
+        return false;
     }
-    //Invoke는 오브젝트의 Action 유무에 관계없이 실행된다.
-    // -> 함수 자체가 딜레이 이후에 실행된다는 이야기이다...?
-    // 그런데 String 형태로 함수를 호출하기에 효율적이지 않다. (느리다는 느낌?)
-    // 그래서 코루틴을 사용했다.
 }
--- Assets/스크립트/오브젝트 상호작용 효과/Rock/Rock_Interplay.cs
+++ Assets/스크립트/오브젝트 상호작용 효과/Rock/Rock_Interplay.cs
@@ -5,25 +5,38 @@ using UnityEngine;
 public class Rock_Interplay : MonoBehaviour , I_Interplay_effect
 {
     AudioSource Audio;
+    float Sound_Value;
     private void Awake()
     {
         //해당 컴포넌트를 Interplay_machice에 참조시킨다.
         GetComponent<Interplay_machice>().SetInterplay(this);
         Audio = GetComponent<AudioSource>();
+        Sound_Value = PlayerPrefs.GetFloat("E_vol_base");
     }
 
     public void Effect() 
     {
-        GameObject.Find("Player").GetComponent<Fire_Bullte>().add_Count_Bullte(1);
-        Destroy(gameObject);
+        if(GameObject.Find("Player").GetComponent<Fire_Bullte>().add_Count_Bullte())
+        {
+            int index = gameObject.name.IndexOf("(Clone)");
+            if (index > 0)
+            {
+                gameObject.name = gameObject.name.Substring(0, index);
+            }
+
+            GameObject.Find("Player").GetComponent<Fire_Bullte>().Bullte = Resources.Load<GameObject>("Rock/" + gameObject.name);
+            Debug.Log(gameObject.name);
+              //  Instantiate(this.gameObject, Vector3.zero, Quaternion.identity);
+            Destroy(gameObject);
+        }
+        
     }
 
     private void OnCollisionEnter(Collision collision)
     {
         if (collision.gameObject.GetComponent<Object_Sound_Clip>())
         {
-            Debug.Log("Play Sound");
-            Audio.PlayOneShot(collision.gameObject.GetComponent<Object_Sound_Clip>().Collider_Sound, PlayerPrefs.GetFloat("E_vol_base"));
+            Audio.PlayOneShot(collision.gameObject.GetComponent<Object_Sound_Clip>().Collider_Sound, 1.0f);
         }
     }
     
