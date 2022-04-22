f241c67e99706558791acd677eb8a651277d8b7c
tjdqls1258 <42854743+tjdqls1258@users.noreply.github.com>
2022-04-22 15:52:17 +0900

    04-22 곽성빈
    연출 연습 2

---
+++ Assets/스크립트/오브젝트 상호작용 효과/Test 스크립트/연출2탄.cs
@@ -0,0 +1,55 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class 연출2탄 : MonoBehaviour
+{
+    [SerializeField] GameObject 따란;
+    [SerializeField] GameObject 빛이_있으라;
+    AudioSource audioSource;
+    int count = 20;
+    bool Is_Atvie = false;
+
+    private void Awake()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (!Is_Atvie)
+        {
+            StartCoroutine(Set_Ative_On());
+            audioSource.Play();
+        }
+    }
+
+    IEnumerator Set_Ative_On()
+    {
+        for(int i = 0; i < count; i++)
+        {
+            yield return new WaitForSeconds(0.05f);
+            if (i == 10)
+            {
+                따란.SetActive(true);
+                Is_Atvie = true;
+                StartCoroutine(Set_Ative_OFF());
+            }
+            if (i % 2 == 1)
+            {
+                빛이_있으라.SetActive(true);
+            }
+            else
+            {
+                빛이_있으라.SetActive(false);
+            }
+        }
+       
+    }
+
+    IEnumerator Set_Ative_OFF()
+    {
+        yield return new WaitForSeconds(0.5f);
+        따란.SetActive(false);
+    }
+}
