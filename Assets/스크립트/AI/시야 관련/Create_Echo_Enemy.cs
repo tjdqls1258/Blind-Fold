01d02ba69addf0128becaae0f0e5a62b4f13911d
tjdqls1258 <42854743+tjdqls1258@users.noreply.github.com>
2022-04-25 13:15:15 +0900

    04-25 곽성빈
    몬스터 에코 플레이어랑 거리 비교

--- Assets/스크립트/AI/시야 관련/Create_Echo_Enemy.cs
+++ Assets/스크립트/AI/시야 관련/Create_Echo_Enemy.cs
@@ -8,13 +8,22 @@ public class Create_Echo_Enemy : MonoBehaviour
     [SerializeField] private float Delay_Time = 2;
     [SerializeField] private float Echo_Power;
     [SerializeField] private float _Time = 0;
+    [SerializeField] private float Max_Create_Echo_Player_And_This_Distance = 30.0f;
+    GameObject Player;
+    private void Awake()
+    {
+        Player = GameObject.Find("Player");
+    }
     void Update()
     {
-        _Time += Time.deltaTime;
-        if(_Time >= Delay_Time)
+        if (Vector3.Distance(Player.transform.position, transform.position) < Max_Create_Echo_Player_And_This_Distance)
         {
-            map_parent.GetComponent<SimpleSonarShader_Parent>().StartSonarRing(transform.position, Echo_Power);
-            _Time = 0;
+            _Time += Time.deltaTime;
+            if (_Time >= Delay_Time)
+            {
+                map_parent.GetComponent<SimpleSonarShader_Parent>().StartSonarRing(transform.position, Echo_Power);
+                _Time = 0;
+            }
         }
     }
 }
