4a990983db10fdd813677eee4cb3f2305294a914
tjdqls1258 <42854743+tjdqls1258@users.noreply.github.com>
2022-04-27 19:40:47 +0900

    04-27 곽성빈
    라디오 1번만  작동수정

--- Assets/스크립트/오브젝트 상호작용 효과/Radio/Radio_Effect.cs
+++ Assets/스크립트/오브젝트 상호작용 효과/Radio/Radio_Effect.cs
@@ -7,6 +7,7 @@ public class Radio_Effect : MonoBehaviour
     [SerializeField] private AudioSource audio;
     [SerializeField] private Text Text;
     [SerializeField] private string Talk;
+    private bool Is_Ative = true;
 
     void Start()
     {
@@ -15,7 +16,7 @@ public class Radio_Effect : MonoBehaviour
 
     private void OnTriggerEnter(Collider other)
     {
-        if(other.tag == "Player")
+        if(other.tag == "Player" && Is_Ative)
         {
             audio.Play();
             Text.text = Talk;
@@ -24,8 +25,9 @@ public class Radio_Effect : MonoBehaviour
 
     private void OnTriggerExit(Collider other)
     {
-        if (other.tag == "Player")
+        if (other.tag == "Player" && Is_Ative)
         {
+            Is_Ative = false;
             audio.Stop();
             Text.text = "";
         }
