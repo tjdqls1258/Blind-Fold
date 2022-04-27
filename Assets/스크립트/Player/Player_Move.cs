c4d4e632b54417cd916ccc832c646e879ce1b2ca
tjdqls1258 <42854743+tjdqls1258@users.noreply.github.com>
2022-04-27 21:02:48 +0900

    04-27 곽성빈
    다시 수정 버그

--- Assets/스크립트/Player/Player_Move.cs
+++ Assets/스크립트/Player/Player_Move.cs
@@ -72,7 +72,6 @@ public class Player_Move : MonoBehaviour
         {
             if (Stamina_Gage >= Min_Stamina_Gage)
             {
-                audioSource.Stop();
                 IsRun = true;
             }
 
