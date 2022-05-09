2d899724c640f3ea5500c18e167a37c9ea3f8c2e
JiWonKimP <86395330+JiWonKimP@users.noreply.github.com>
2022-05-09 20:18:59 +0900

    22/05/09 - 01
    스크립트 일부 수정

--- Assets/스크립트/UI/DialogueSystem.cs
+++ Assets/스크립트/UI/DialogueSystem.cs
@@ -22,8 +22,8 @@ public class DialogueSystem : MonoBehaviour
             sentences.Enqueue(sentence);
         }
 
-        //Next();
-        StartCoroutine(Next_script());
+        Next();
+        //StartCoroutine(Next_script());
     }
 
     public void Next()
@@ -52,7 +52,16 @@ public class DialogueSystem : MonoBehaviour
 
     IEnumerator Next_script()
     {
-        yield return new WaitForSeconds(3.0f);
+        float fadecount = 1.0f;
+        yield return new WaitForSeconds(1.0f);
+
+        while (fadecount > 0.0f)
+        {
+            fadecount -= 0.03f;
+            testtext.color = new Color(255, 255, 255, fadecount);
+            yield return new WaitForSeconds(0.1f);
+        }
+
         Next();
     }
 }
