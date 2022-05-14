e9817c8dea5b272c9e7a57f2bfc1c3df7ae690bc
tjdqls1258 <42854743+tjdqls1258@users.noreply.github.com>
2022-05-14 21:02:00 +0900

    05-14 곽성빈
    스테이지 시작연출 예시

---
+++ Assets/스크립트/Test/Post_Bloom_Con.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering.PostProcessing;
+
+public class Post_Bloom_Con : MonoBehaviour
+{
+    PostProcessVolume post;
+    // Start is called before the first frame update
+    void Awake()
+    {
+        post = GetComponent<PostProcessVolume>();
+        StartCoroutine(Change_Vaule_To_Bloom());
+    }
+    IEnumerator Change_Vaule_To_Bloom()
+    {
+        while (post.profile.GetSetting<Bloom>().intensity.value >= 0.0f)
+        {
+            post.profile.GetSetting<Bloom>().intensity.value -= Time.deltaTime * 30.0f;
+            yield return null;
+        }
+        yield return null;
+    }
+}
