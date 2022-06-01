4a7470b1e4e473daf00478e3944a05d5bcd60f82
JiWonKimP <86395330+JiWonKimP@users.noreply.github.com>
2022-06-01 23:50:09 +0900

    22/06/01-06
    4스테이지 ui 연결오류 수정

--- Assets/스크립트/Test/Password_Locker.cs
+++ Assets/스크립트/Test/Password_Locker.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 using UnityEngine.UI;
 public class Password_Locker : MonoBehaviour , I_Interplay_effect
 {
-    [SerializeField] private GameObject Locker;
+    [SerializeField] private GameObject PasswordUI;
     [SerializeField] private GameObject UI;
     public bool Is_Open = false;
 
@@ -19,7 +19,7 @@ public class Password_Locker : MonoBehaviour , I_Interplay_effect
         {
             return;
         }
-        Locker.SetActive(true);
+        PasswordUI.SetActive(true);
         UI.GetComponent<Play_UI>().isdie = true;
         GameObject.Find("Player").GetComponent<Fire_Bullte>().enabled = false;
         Time.timeScale = 0.0f;
