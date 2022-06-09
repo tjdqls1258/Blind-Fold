48a6585a0de273eab0b8d5d310489ff1b3284e81
tjdqls1258 <42854743+tjdqls1258@users.noreply.github.com>
2022-06-09 21:27:22 +0900

    06-09 곽성빈
    2스테이지 문 연출 버그 수정,
    3스테이지 연출 일부 수정

--- Assets/스크립트/연출/Stage3/Stage4_04_start.cs
+++ Assets/스크립트/연출/Stage3/Stage4_04_start.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class Stage4_04_start : MonoBehaviour
 {
-    [SerializeField] private GameObject Key, Ghost, Blood;
+    [SerializeField] private GameObject Key, Ghost, Blood, PlayerHead;
     private Animator anim;
     private bool Is_Coll = false;
 
@@ -19,16 +19,18 @@ public class Stage4_04_start : MonoBehaviour
         {
             if(!Key && !Is_Coll)
             {
+                PlayerHead.GetComponent<Kino.AnalogGlitch>().scanLineJitter = 0.5f;
                 Is_Coll = true;
-                StartCoroutine(Set_Ative());
+                StartCoroutine(Set_Ative(other));
             }
         }
     }
 
-    IEnumerator Set_Ative()
+    IEnumerator Set_Ative(Collider other)
     {
         Ghost.SetActive(true);
         yield return new WaitForSeconds(0.4f);
+        PlayerHead.GetComponent<Kino.AnalogGlitch>().scanLineJitter = 0.0f;
         Blood.SetActive(true);
 
         yield return new WaitForSeconds(5.0f);
