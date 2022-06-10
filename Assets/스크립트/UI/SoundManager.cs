57dde8c252e53a6dba1bc545308a8f0b655c6639
tjdqls1258 <42854743+tjdqls1258@users.noreply.github.com>
2022-06-10 23:44:15 +0900

    06-10 곽성빈
    사운드 오류 수정

--- Assets/스크립트/UI/SoundManager.cs
+++ Assets/스크립트/UI/SoundManager.cs
@@ -18,12 +18,13 @@ public class SoundManager : MonoBehaviour
 
     private bool m_toggle = true;
     private bool e_toggle = true;
+    private bool Is_Awake = true;
 
     // Start is called before the first frame update
     void Start()
     {
+        E_vol_base = PlayerPrefs.GetFloat("Effect_vol_base", 1.0f);
         M_vol_base = PlayerPrefs.GetFloat("M_vol_base", 1.0f);
-        E_vol_base = PlayerPrefs.GetFloat("E_vol_base", 1.0f);
 
         MusicVolume.value = M_vol_base;
         EffectVolume.value = E_vol_base;
@@ -34,12 +35,21 @@ public class SoundManager : MonoBehaviour
 
     public void SoundSetting()
     {
+        if(Is_Awake)
+        {
+            Is_Awake = false;
+            return;
+        }
+        Debug.Log("Ω«ùÓ");
         M_vol_base = MusicVolume.value;
         E_vol_base = EffectVolume.value;
 
+        PlayerPrefs.SetFloat("M_vol_base", M_vol_base);
+        PlayerPrefs.SetFloat("Effect_vol_base", E_vol_base);
+
         if (M_vol_base <= 0.01f)
         {
-            audio.SetFloat("musicVolume", (M_vol_base * 60.0f) - 80.0f);
+            audio.SetFloat("musicVolume", -80.0f);
         }
         else
         {
@@ -48,16 +58,11 @@ public class SoundManager : MonoBehaviour
 
         if (E_vol_base <= 0.01f)
         {
-            audio.SetFloat("sfxVolume", (E_vol_base * 60.0f) - 80.0f);
+            audio.SetFloat("sfxVolume", -80.0f);
         }
         else
         {
             audio.SetFloat("sfxVolume", (E_vol_base * 60.0f) - 40.0f);
         }
-
-        PlayerPrefs.SetFloat("M_vol_base", M_vol_base);
-        PlayerPrefs.SetFloat("E_vol_base", E_vol_base);
-        Debug.Log(M_vol_base);
-        Debug.Log(E_vol_base);
     }
 }
